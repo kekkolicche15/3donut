ee4dd0e69d839c84e17bb55c94e8d8071e8e93f4
Francesco Licchelli <francesco.licchelli@studio.unibo.it>
2022-12-01 01:47:08 +0100

    peppe

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ all: compile run
 
 compile:
 	mkdir -p bin
-	gcc ./source/main.c -o ./bin/main -lm
+	gcc -g ./source/main.c -o ./bin/main -lm
 
 run:
 	./bin/main
--- source/main.c
+++ source/main.c
@@ -13,27 +13,31 @@ double sqrt(double);
 typedef unsigned char uint8_t;
 typedef char int8_t;
 
-#define RAD_CHECK 2
+#define RAD_CHECK 1
 
-const char* chars = " `'.^~_\",-:;\\/!|*+()=><7[]1?il{}rtIvjJcu3znoL25Yf4sCVx&a0Teh96FyZwkUP$ODS@8XbGAgdKmREHpqNB#WQM#WQM#WQM#WQM#WQM#WQM{}rtIvjJcu3znoL25Yf4sCVx&a0Teh96FyZwkUP$ODS@8XbGAgdKmREHpqNB#WQM#WQM#WQM#WQM#WQM#WQM{}rtIvjJcu3znoL25Yf4sCVx&a0Teh96FyZwkUP$ODS@8XbGAgdKmREHpqNB#WQM#WQM#WQM#WQM#WQM#WQM{}rtIvjJcu3znoL25Yf4sCVx&a0Teh96FyZwkUP$ODS@8XbGAgdKmREHpqNB#WQM#WQM#WQM#WQM#WQM#WQM{}rtIvjJcu3znoL25Yf4sCVx&a0Teh96FyZwkUP$ODS@8XbGAgdKmREHpqNB#WQM#WQM#WQM#WQM#WQM#WQM{}rtIvjJcu3znoL25Yf4sCVx&a0Teh96FyZwkUP$ODS@8XbGAgdKmREHpqNB#WQM#WQM#WQM#WQM#WQM#WQM{}rtIvjJcu3znoL25Yf4sCVx&a0Teh96FyZwkUP$ODS@8XbGAgdKmREHpqNB#WQM#WQM#WQM#WQM#WQM#WQM{}rtIvjJcu3znoL25Yf4sCVx&a0Teh96FyZwkUP$ODS@8XbGAgdKmREHpqNB#WQM#WQM#WQM#WQM#WQM#WQM{}rtIvjJcu3znoL25Yf4sCVx&a0Teh96FyZwkUP$ODS@8XbGAgdKmREHpqNB#WQM#WQM#WQM#WQM#WQM#WQM{}rtIvjJcu3znoL25Yf4sCVx&a0Teh96FyZwkUP$ODS@8XbGAgdKmREHpqNB#WQM#WQM#WQM#WQM#WQM#WQM{}rtIvjJcu3znoL25Yf4sCVx&a0Teh96FyZwkUP$ODS@8XbGAgdKmREHpqNB#WQM#WQM#WQM#WQM#WQM#WQM";
-// const char* chars = " `*O@#";
-//    const char* chars = " oO0@$";
+const char* chars = " `'.^~_\",-:;\\/!|*+()=><7[]1?il{}rtIvjJcu3znoL25Yf4sCVx&a0Teh96FyZwkUP$ODS@8XbGAgdKmREHpqNB#WQM";
+int i, j, k;
+int l, m, n; //suqqiami lo scroto best practices
 
+void printDonut(uint8_t*** donut){
+    for(i=0; i<SIZE; i++, usleep(SLEEP_uS), printf("\033[2J\033[1;1H"))
+        for(j = 0; j<SIZE; j++, printf("|\n"))
+            for(k = 0; k<SIZE; k++) 
+                printf(" %c ", chars[donut[j][k][i]]);
+}
 
 int min(int a, int b) {
     return a < b ? a : b;
 }
 
 
+
 void main(void){
     if(SIZE>=LIMIT)
         exit(-1);
-    
     _Bool ***donut = calloc(sizeof(void*), SIZE);
     uint8_t ***screen = calloc(sizeof(void*), SIZE);
     double dist;
-    int i, j, k;
-    int l, m, n;
     for(i=0; i<SIZE; i++){
         donut[i] = calloc(sizeof(void*), SIZE);
         screen[i] = calloc(sizeof(void*), SIZE);
@@ -43,31 +47,61 @@ void main(void){
             for(k=0; k<SIZE; k++){
                 dist = sqrt(i*i+j*j+2*SIZE/2*(SIZE/2-i-j));
                 donut[i][j][k] = sqrt((dist-SIZE/SHORTNESS)*(dist-SIZE/SHORTNESS)+(k-SIZE/2)*(k-SIZE/2))<=SIZE/SLIMNESS;
-                    // if(l+k<SIZE && l+k>=0 && donut[i][j][k+l])
-                    //     screen[i][j][k]++;
             }
         }
     }
-    for(i=0; i<SIZE; i++){
-        for(j=0; j<SIZE; j++){
-            for(k=0; k<SIZE; k++){
-                for(l=-RAD_CHECK; l<=RAD_CHECK; l++){
-                    for(m=-RAD_CHECK; m<=RAD_CHECK; m++){
-                        for(n=-RAD_CHECK; n<=RAD_CHECK; n++){
-                            if(i+l<SIZE && i+l>=0 && j+m<SIZE && j+m>=0 && k+n<SIZE && k+n>=0 && donut[i+l][j+m][k+n])
-                                screen[i][j][k]++;
-                        }
+
+    uint8_t ***prefix = calloc(sizeof(void*), SIZE + RAD_CHECK * 2 + 1);
+    for (int i = 0; i < SIZE + RAD_CHECK * 2 + 1; i++) {
+        prefix[i] = calloc(sizeof(void*), SIZE + RAD_CHECK * 2 + 1);
+        for (int j = 0; j < SIZE + RAD_CHECK * 2 + 1; j++) {
+            prefix[i][j] = calloc(sizeof(uint8_t), SIZE + RAD_CHECK * 2 + 1);
+        }
+    }
+
+    for (int i = 0; i < SIZE + RAD_CHECK * 2 + 1; i++) {
+        for (int j = 0; j < SIZE + RAD_CHECK * 2 + 1; j++) {
+            for (int k = 0; k < SIZE + RAD_CHECK * 2 + 1; k++) {
+                if (i <= RAD_CHECK || j <= RAD_CHECK || k <= RAD_CHECK ) {
+                    prefix[i][j][k] = 0;
+                } else {
+                    if (i > SIZE + RAD_CHECK || j > SIZE + RAD_CHECK || k > SIZE + RAD_CHECK) {
+                        prefix[i][j][k] = 0;
+                    } else {
+                        prefix[i][j][k] = donut[i-RAD_CHECK-1][j-RAD_CHECK-1][k-RAD_CHECK-1];
                     }
+                    prefix[i][j][k] += prefix[i-1][j][k] + prefix[i][j-1][k] + prefix[i][j][k-1] - prefix[i-1][j-1][k] - prefix[i-1][j][k-1] - prefix[i][j-1][k-1] + prefix[i-1][j-1][k-1];
                 }
-            }
+            } 
         }
     }
-    for(i=0; i<SIZE; i++, usleep(SLEEP_uS), printf("\033[2J\033[1;1H"))
-        for(j = 0; j<SIZE; j++, printf("|\n"))
-            for(k = 0; k<SIZE; k++) 
-                printf(" %c ", chars[screen[j][k][i]]);
+    for (int i = 0; i < SIZE; i++) 
+        for (int j = 0; j < SIZE; j++) 
+            for (int k = 0; k < SIZE; k++) 
+                screen[i][j][k] = prefix[i+2*RAD_CHECK+1][j+2*RAD_CHECK+1][k+2*RAD_CHECK+1] 
+                                    - prefix[i][j+2*RAD_CHECK+1][k+2*RAD_CHECK+1] 
+                                    - prefix[i+2*RAD_CHECK+1][j][k+2*RAD_CHECK+1] 
+                                    - prefix[i+2*RAD_CHECK+1][j+2*RAD_CHECK+1][k] 
+                                    + prefix[i][j][k+2*RAD_CHECK+1] 
+                                    + prefix[i][j+2*RAD_CHECK+1][k] 
+                                    + prefix[i+2*RAD_CHECK+1][j][k] 
+                                    - prefix[i][j][k];
+    
     for(i=0; i<SIZE; i++, usleep(SLEEP_uS), printf("\033[2J\033[1;1H"))
         for(j = 0; j<SIZE; j++, printf("|\n"))
             for(k = 0; k<SIZE; k++) 
                 printf(" %c ", chars[screen[i][j][k]+screen[i][k][j]+screen[j][k][i]]);
+
+    printDonut(screen);
 }
+/*
+
+    TODO 
+    printDonut(donut, 1, 1, 1, +, +, +, |, |);
+    void printDonut(uint8_t*** donut){
+        for(i=0; i<SIZE; i++, usleep(SLEEP_uS), printf("\033[2J\033[1;1H"))
+            for(j = 0; j<SIZE; j++, printf("|\n"))
+                for(k = 0; k<SIZE; k++) 
+                    printf(" %c ", chars[expr(donut[i][j][k], donut[i][k][j], donut[j][k][i])]);
+    }
+*/
